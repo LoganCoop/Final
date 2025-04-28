cc08d1c031565e08cd82deacfe86b528a8a8e379
LoganCoop <lpcooper@gmail.com>
2025-04-28 14:12:53 -0400

    added in dome eslint disables

--- client/fitness-tracker/src/App.vue
+++ client/fitness-tracker/src/App.vue
@@ -1,3 +1,4 @@
+/* eslint-disable */
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { onMounted } from 'vue'
--- client/fitness-tracker/src/views/admin.vue
+++ client/fitness-tracker/src/views/admin.vue
@@ -1,3 +1,4 @@
+/* eslint-disable */
 <template>
     <div class="admin-view">
         <h1>Admin Panel</h1>
