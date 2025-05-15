5e3e998131671e4785fac29bcd329856636a1e6d
LoganCoop <lpcooper@gmail.com>
2025-05-15 13:46:32 -0400

    Fixes

--- client/fitness-tracker/src/main.ts
+++ client/fitness-tracker/src/main.ts
@@ -8,6 +8,7 @@ import 'vuetify/styles';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
 
 
 const userToken = localStorage.getItem('userToken')
